9e412fa3eba720cac366a03acbed1eede5bbfe6f
Fatma Erturk <>
2022-10-27 22:40:50 +0300

    Update Q12TestLogic

--- testcase/Q12TestLogic.java
+++ testcase/Q12TestLogic.java
@@ -9,7 +9,7 @@ public class Q12TestLogic {
     int num2 = 58; String output2="LVIII";
     int num3 = 1994; String output3="MCMXCIV";
     @Test
-    public void findReverseInteger() {
+    public void findIntegertoRoman() {
         assertEquals(output1, Q12IntegertoRoman.intToRoman(num1));
         assertEquals(output2, Q12IntegertoRoman.intToRoman(num2));
         assertEquals(output3, Q12IntegertoRoman.intToRoman(num3));
