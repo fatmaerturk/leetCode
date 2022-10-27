8609d0d5f3631f89cf6fa9f39e824fb2b1650fc8
Fatma Erturk <>
2022-10-27 22:24:30 +0300

    test cases Q12IntegertoRoman

---
+++ testcase/Q12TestLogic.java
@@ -0,0 +1,18 @@
+package testcase;
+import logic.Q12IntegertoRoman;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class Q12TestLogic {
+    int num1 = 3; String output1="III";
+    int num2 = 58; String output2="LVIII";
+    int num3 = 1994; String output3="MCMXCIV";
+    @Test
+    public void findReverseInteger() {
+        assertEquals(output1, Q12IntegertoRoman.intToRoman(num1));
+        assertEquals(output2, Q12IntegertoRoman.intToRoman(num2));
+        assertEquals(output3, Q12IntegertoRoman.intToRoman(num3));
+    }
+}
+
