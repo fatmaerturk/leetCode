bba24a3c7e53f01b8ab5545499d28f21db76cfe8
Fatma Erturk <>
2022-10-28 06:53:28 +0300

    Update Q88MergeSortedArray.java

--- DataStructure1/logic/Q88MergeSortedArray.java
+++ DataStructure1/logic/Q88MergeSortedArray.java
@@ -6,19 +6,14 @@ import java.util.Arrays;
 import static java.util.Arrays.*;
 
 public class Q88MergeSortedArray {
-    public static void main(String[] args) {
-        int[] nums1_1 = {1,2,3,0,0,0}, nums1_2 = {2,5,6}; int[] output1={1,2,2,3,5,6}; int m1 = 3, n1 = 3;
-
-      merge(nums1_1, m1,nums1_2, n1);
-    }
     public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
-        if(nums1.length!=(m+n)) return null;
-        for (int j=0; j<n; j++){
-            nums1[j+m]=nums2[j];
+        if (nums1.length != (m + n)) return null;
+        for (int j = 0; j < n; j++) {
+            nums1[j + m] = nums2[j];
         }
-        nums2=new int[n];
-        for (int j=0; j<n; j++){
-            nums2[j]=nums1[j+m];
+        nums2 = new int[n];
+        for (int j = 0; j < n; j++) {
+            nums2[j] = nums1[j + m];
         }
         Arrays.sort(nums1);
         return nums1;
